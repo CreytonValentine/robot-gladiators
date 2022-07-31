2eb50f053d0575d71aba9354910fc470ce1adaa9
CreyValentine <valentine.crey95@gmail.com>
2022-07-31 01:30:02 -0500

    console.log API

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,18 @@
 var playerName = window.prompt("What is your robot's name?");
 
+// Note the lack of quotation marks around playerName
+//window.alert(playerName);
+
+console.log(playerName);
+
+// What is this?
+console.log("This logs a string, good for leaving yourself a message");
+
+// this will do math and log 20
+console.log(10+10);
+
+// what is this?
+console.log("our robot's name is " + playerName)
 
 function fight() {
     window.alert("The fight has begun");
