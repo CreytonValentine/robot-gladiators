40a1a55efecd935cd5454c3d829a31c15515e5e5
CreyValentine <valentine.crey95@gmail.com>
2022-08-01 04:56:23 -0500

    extended rounds

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,10 +7,9 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     // IF player chooses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -59,9 +58,11 @@ var fight = function(enemyName) {
                 fight();
             }
         }
+    }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
     fight(enemyNames[i])
 }
-
